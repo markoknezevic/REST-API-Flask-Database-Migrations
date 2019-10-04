0774351714fef90010e90c37a544035c3aea7e07
Marko Knezevic <markoknezevic75@gmail.com>
2019-10-04 15:10:09 +0200

    Adding A New Column With Migrations
    Adding A New Column With Migrations

--- models/user.py
+++ models/user.py
@@ -7,7 +7,8 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    
     @classmethod
     def find_by_username(cls, username: str) -> "UserModel":
         return cls.query.filter_by(username=username).first()
