f9d7f09ab69b00e75ec10b4d16aaab0fb59c0871
Marko Knezevic <markoknezevic75@gmail.com>
2019-10-04 15:59:35 +0200

    Deleted migrations  folder

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ class UserModel(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False, unique=True)
+    username = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80), nullable=False, unique=True)
     
